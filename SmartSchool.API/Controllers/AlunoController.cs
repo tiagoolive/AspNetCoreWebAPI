a9981cab0a83c9ae90279687286114531b74a4f0
tiago camargo de oliveira <tiago.olive.cam@hotmail.com>
2021-02-22 14:24:20 -0300

    Adicionar arquivos de projeto.

---
+++ SmartSchool.API/Controllers/AlunoController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SmartSchool.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AlunoController : ControllerBase
+    {
+        public List<Aluno> Alunos = new List<Aluno>()
+        {
+            new Aluno()
+            {
+                Id = 1,
+                Nome = "Tiago",
+                Sobrenome = "Camargo",
+                Telefone = "787239848",
+            },
+            new Aluno()
+            {
+                Id = 2,
+                Nome = "Karol",
+                Sobrenome = "Conka",
+                Telefone = "587364242",
+            },
+            new Aluno()
+            {
+                Id = 3,
+                Nome = "Mateus",
+                Sobrenome = "Fernando",
+                Telefone = "7837457732",
+            },
+            new Aluno()
+            {
+                Id = 4,
+                Nome = "Yuri",
+                Sobrenome = "Nascimento",
+                Telefone = "687839584",
+            }
+        };
+        public AlunoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Alunos);
+        }
+
+        [HttpGet("ById/{id}")]
+        public IActionResult GetById(int id)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Id == id);
+            if (aluno == null) return BadRequest("aluno não encontrado");
+
+            return Ok(aluno);
+        }
+
+        [HttpGet("ByName")]
+        public IActionResult GetByName(string nome)
+        {
+            var aluno = Alunos.FirstOrDefault(a => a.Nome.Contains(nome));
+            if (aluno == null) return BadRequest("aluno não encontrado");
+
+            return Ok(aluno);
+        }
+        [HttpPost]
+        public IActionResult Post(Aluno aluno)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(Aluno aluno, int id)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(Aluno aluno, int id)
+        {
+            return Ok(aluno);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SmartSchool.API/Controllers/ProfessorController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        public ProfessorController()
+        {
+
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Professores: Carlos, joão, Mikaela, Fernando");
+        }
+    }
+}
